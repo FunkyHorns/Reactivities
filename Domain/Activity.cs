29745b8feab4fadff7cd5dde72aca215e7dd1274
Lance Joshua Hilario <lance.joshua19@gmail.com>
2025-12-13 17:30:55 +0800

    Adding models

---
+++ Domain/Activity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCancelled { get; set; }
+
+        //location props
+
+        public string City { get; set; }
+
+        public string Venue { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+    }
+}
