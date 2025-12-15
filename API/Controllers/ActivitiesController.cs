daa3ca0b9d9fe6e7bfb669c7e6685ce3da4b6a2c
Lance Joshua Hilario <lance.joshua19@gmail.com>
2025-12-15 19:20:27 +0800

    additional

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System
+
+
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+}
+
